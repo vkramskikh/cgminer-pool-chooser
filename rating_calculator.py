2b2177d8e824e92c0e835000b1657b41f10b408c
Vitaly Kramskikh <vkramskikh@mirantis.com>
2013-12-23 00:18:56 +0400

    analyze_difficulty rate method

--- rating_calculator.py
+++ rating_calculator.py
@@ -6,6 +6,12 @@ logger = logging.getLogger(__name__)
 class RatingCalculator(object):
 
     @staticmethod
+    def analyze_exchange_volume(currency):
+        exchange_ratio = currency['exchange_ratio'] = currency['exchange_volume'] / currency['coins_per_day']
+        exchange_volume_change = (exp(-1000.0 / exchange_ratio) - 0.6) / 2
+        return exchange_volume_change
+
+    @staticmethod
     def analyze_profit_growth(currency):
         profit_growth = currency['profit_growth']
         profit_growth_change = 0
@@ -14,17 +20,17 @@ class RatingCalculator(object):
         return profit_growth_change
 
     @staticmethod
-    def analyze_exchange_volume(currency):
-        exchange_ratio = currency['exchange_ratio'] = currency['exchange_volume'] / currency['coins_per_day']
-        exchange_volume_change = (exp(-1000.0 / exchange_ratio) - 0.6) / 2
-        return exchange_volume_change
+    def analyze_difficulty(currency):
+        difficulty = currency['difficulty']
+        difficulty_change = -exp(-200.0 / difficulty) / 5
+        return difficulty_change
 
     @classmethod
     def rate_currency(cls, currency):
         from math import exp
         rating = currency['usd_per_day']
         logger.debug('%s original rating is %f', currency['name'], rating)
-        for method in ('analyze_exchange_volume', 'analyze_profit_growth'):
+        for method in ('analyze_exchange_volume', 'analyze_profit_growth', 'analyze_difficulty'):
             rating_change = getattr(cls, method)(currency)
             rating *= (rating_change + 1)
             logger.debug('%s rating changed by %s by %.2f%% to %f', currency['name'], method, rating_change * 100, rating)
