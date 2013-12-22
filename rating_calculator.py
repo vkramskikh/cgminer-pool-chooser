925f1c9ff0cc2d7d3a281ac37751a5dba91bdfac
Vitaly Kramskikh <vkramskikh@mirantis.com>
2013-12-23 00:31:36 +0400

    Comments for analyze_ methods

--- rating_calculator.py
+++ rating_calculator.py
@@ -7,12 +7,14 @@ class RatingCalculator(object):
 
     @staticmethod
     def analyze_exchange_volume(currency):
+        # price of coins with low exchange volume is usually not stable, reduce rating
         exchange_ratio = currency['exchange_ratio'] = currency['exchange_volume'] / currency['coins_per_day']
         exchange_volume_change = (exp(-1000.0 / exchange_ratio) - 0.6) / 2
         return exchange_volume_change
 
     @staticmethod
     def analyze_profit_growth(currency):
+        # price of coins with profit grow ratio > 1.5 is not stable, reduce rating
         profit_growth = currency['profit_growth']
         profit_growth_change = 0
         if profit_growth > 1:
@@ -21,6 +23,7 @@ class RatingCalculator(object):
 
     @staticmethod
     def analyze_difficulty(currency):
+        # coins with high difficulty don't let me switch PPLNS pools frequently, slightly reduce rating
         difficulty = currency['difficulty']
         difficulty_change = -exp(-200.0 / difficulty) / 5
         return difficulty_change
